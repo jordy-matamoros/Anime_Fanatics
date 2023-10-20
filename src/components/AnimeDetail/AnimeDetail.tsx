55b68e2d29c7cb41b7a547ec64fef5221fff1f5e
Jordy <jordyfmm@gmail.com>
2023-10-20 18:01:32 -0500

    Updated Anime Detail

--- src/components/AnimeDetail/AnimeDetail.tsx
+++ src/components/AnimeDetail/AnimeDetail.tsx
@@ -29,16 +29,53 @@ export const AnimeDetail = () => {
                     color: "#2f353a", 
                     fontSize: "20px", 
                     textDecoration: "none"
-                }} to="/dashboard">&lt;Back</Link>
+                }} to="/dashboard"><strong>&lt; Back</strong></Link>
             </nav>
             {anime !== null ? (
-                <div>
-                    <h2>{anime.data.title}</h2>
-                    <img src={anime.data.images.jpg.image_url} alt={anime.data.title} />
-                    <p>{anime.data.synopsis}</p>
-                    <p>GENRES: {anime.data.genres.map((genre: { name: string }) => (
-                        genre.name + ", "
-                    ))}</p>
+                <div style={{margin: "20px", display: "flex", flexDirection: "row", backgroundColor: "#e3e3e3"}}>
+                    <nav style={{borderRight: "1px solid #2f353a", padding: "10px",}}>
+                        <h2 style={{marginTop: "0px"}}>{anime.data.title}</h2>
+                        <img src={anime.data.images.jpg.image_url} alt={anime.data.title} />
+                        <h3 style={{borderBottom: "1px solid #2f353a"}}>Information</h3>
+                        <p> <strong>Type:</strong> {anime.data.type}</p>
+                        <p> <strong>Episodes:</strong> {anime.data.episodes}</p>
+                        <p> <strong>Status:</strong> {anime.data.status}</p>
+                        <p><strong>Genres:</strong> {anime.data.genres.map((genre: { name: string }) => (
+                            genre.name + ", "
+                        ))}
+                        </p>
+                        <p><strong>Duration:</strong> {anime.data.duration}</p>
+                        <p><strong>Rating:</strong> {anime.data.rating}</p>
+                    </nav>
+                    <div style={{margin: "10px"}}>
+                        <nav style={{
+                            border: "1px solid #2f353a", 
+                            display: "flex", 
+                            flexDirection: "row", 
+                            justifyContent: "space-around",
+                            color: "#e0e5e9",
+                            backgroundColor: "#2f353a",
+                            }}>
+                            <h2 style={{margin: "0px"}}>Score: {anime.data.score}★</h2>
+                            <h2 style={{margin: "0px"}}>Ranked: #{anime.data.rank}</h2>
+                            <h2 style={{margin: "0px"}}>Popularity: #{anime.data.popularity}</h2>
+                        </nav>
+                        <h3 style={{ borderBottom: "1px solid #2f353a"}}>Trailer</h3>
+                        <a href={anime.data.trailer.url}>
+                            <img src={anime.data.trailer.images.medium_image_url} 
+                            alt={anime.data.title}/>
+                        </a>
+                        <h3 style={{ borderBottom: "1px solid #2f353a"}}>Synopsis</h3>
+                        <p>{anime.data.synopsis}</p>
+                        <h3 style={{ borderBottom: "1px solid #2f353a"}}>Background</h3>
+                        <p>{anime.data.background}</p>
+                        <h3 style={{ borderBottom: "1px solid #2f353a"}}>More Information</h3>
+                        <p>For more information visit:
+                            <a style={{color: "#2f353a", textDecoration: "none"}} 
+                                href={anime.data.url}>  <strong>MyAnimeList ➡︎ {anime.data.title}</strong>
+                            </a>
+                        </p>
+                    </div>
                 </div>
             ): (
                 <NotFound />
